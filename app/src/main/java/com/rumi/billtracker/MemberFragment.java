dac8a6db41385058d3293972fd39dfa189b1b881
merkercana <cana.merker@gmail.com>
2016-03-26 00:51:44 -0400

    Complete 'Add Member to group' Functionality
    with robustness!

--- app/src/main/java/com/rumi/billtracker/MemberFragment.java
+++ app/src/main/java/com/rumi/billtracker/MemberFragment.java
@@ -10,9 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import android.widget.Toast;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -22,6 +32,14 @@ public class MemberFragment extends android.support.v4.app.Fragment {
 
     public Button btn_addMember;
     public EditText etb_addMemberName;
+
+    static Map<String, Object> user;
+    static Map<String, Object> addMember;
+    static Map<String, Object> newUser;
+    static String message;
+    static boolean error;
+    static boolean validated;
+
 
     public MemberFragment() {
         // Required empty public constructor
@@ -44,20 +62,108 @@ public class MemberFragment extends android.support.v4.app.Fragment {
         SharedPreferences sharedPreferences = getContext().getSharedPreferences(Utility.GROUP_NAME, Context.MODE_PRIVATE);
         final String groupName = sharedPreferences.getString(Utility.GROUP_NAME, null);
 
-        btn_addMember = (Button)v.findViewById(R.id.btn_addMember);
-        etb_addMemberName = (EditText)v.findViewById(R.id.etb_addMemberName);
+        btn_addMember = (Button) v.findViewById(R.id.btn_addMember);
+        etb_addMemberName = (EditText) v.findViewById(R.id.etb_addMemberName);
 
+        //region Add Member
         btn_addMember.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = Utility.addMember(rootRef, groupName, etb_addMemberName.getText().toString());
-                Toast.makeText(getContext(),message,Toast.LENGTH_LONG).show();
+                final String member = etb_addMemberName.getText().toString();
+                validated = false;
+                error = false;
+
+                Query validateRef = rootRef.child("Users");
+                validateRef.orderByKey();
+                validateRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(final DataSnapshot dataSnapshot) {
+                        if (!validated)
+                            if (dataSnapshot.hasChild(rootRef.child("Users").child(member).getKey())) {
+                                Query validateGroupRef = rootRef.child("Users").child(member);
+                                validateGroupRef.addValueEventListener(new ValueEventListener() {
+
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot2) {
+                                        if (!validated)
+                                            if (!dataSnapshot2.hasChild(rootRef.child("Users").child(member).child(groupName).getKey()))/*.equals(groupName))*/ {
+                                                Toast.makeText(getActivity(), member + " has been added to " + groupName, Toast.LENGTH_SHORT).show();
+
+                                                validated = true;
+                                                user = new HashMap<String, Object>();
+                                                user.put(member, 0);
+                                                rootRef.child("Groups").child(groupName).child("Members").updateChildren(user);
+
+                                                Query queryRef = rootRef.child("Groups").child(groupName).child("Members");
+                                                queryRef.addChildEventListener(new ChildEventListener() {
+
+                                                    @Override
+                                                    public void onChildAdded(DataSnapshot dataSnapshot3, String s) {
+                                                        if (!dataSnapshot3.getKey().equals(member)) {
+                                                            addMember = new HashMap<String, Object>();
+                                                            addMember.put(dataSnapshot3.getKey().toString(), 0);
+                                                            rootRef.child("Users").child(member).child(groupName).child("MemberRelations").updateChildren(addMember);
+
+                                                            newUser = new HashMap<String, Object>();
+                                                            newUser.put(member, 0);
+                                                            rootRef.child("Users").child(dataSnapshot3.getKey()).child(groupName).child("MemberRelations").updateChildren(newUser);
+                                                            error = false;
+                                                        }
+
+                                                    }
+
+                                                    //region Override Functions
+                                                    @Override
+                                                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                                                    }
+
+                                                    @Override
+                                                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                                                    }
+
+                                                    @Override
+                                                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                                                    }
+
+                                                    @Override
+                                                    public void onCancelled(FirebaseError firebaseError) {
+
+                                                    }
+                                                    //endregion
+
+                                                });
+                                            }
+                                            else {
+                                                Toast.makeText(getActivity(), "The specified user is already part of the group.", Toast.LENGTH_SHORT).show();
+                                                error = true;
+                                            }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(FirebaseError firebaseError) {
+
+                                    }
+                                });
+
+                            } else {
+                                Toast.makeText(getActivity(), "The specified user does not exist.", Toast.LENGTH_SHORT).show();
+                                error = true;
+                            }
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+                });
             }
         });
+        //endregion
 
         // Inflate the layout for this fragment
         return v;
     }
-
-
 }
--- app/src/main/java/com/rumi/billtracker/Utility.java
+++ app/src/main/java/com/rumi/billtracker/Utility.java
@@ -1,6 +1,7 @@
 package com.rumi.billtracker;
 
 
+import android.content.Context;
 import android.widget.Toast;
 
 import com.firebase.client.ChildEventListener;
@@ -24,6 +25,7 @@ public class Utility {
     static Map<String, Object> user;
     static String message;
     static boolean error;
+    static boolean validated;
 
     public static void createGroup(Firebase rootRef, String groupName, String groupCreator) {
         rootRef.child("Groups").child(groupName).child("Bills").setValue("");
@@ -34,76 +36,85 @@ public class Utility {
         rootRef.child("Users").child(groupCreator).child(groupName).child("Net").setValue(0);
     }
 
-    public static String addMember(final Firebase rootRef, final String groupName, final String member) {
-
+    public static void addMember(final Firebase rootRef, final String groupName, final String member/*, final Context context*/) {
+        validated = false;
         error = false;
         Query validateRef = rootRef.child("Users");
         validateRef.orderByKey();
         validateRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.hasChild(rootRef.child("Users").child(member).getKey())) {
-                    Query validateGroupRef = rootRef.child("Users").child(member);
-                    validateGroupRef.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(final DataSnapshot dataSnapshot) {
+                if (!validated)
+                    if (dataSnapshot.hasChild(rootRef.child("Users").child(member).getKey())) {
+                        Query validateGroupRef = rootRef.child("Users").child(member);
+                        validateGroupRef.addValueEventListener(new ValueEventListener() {
 
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot2) {
-                            if (!dataSnapshot2.hasChild(rootRef.child("Users").child(member).child(groupName).getKey())) {
-                                user = new HashMap<String, Object>();
-                                user.put(member, 0);
-                                rootRef.child("Users").child(member).child(groupName).setValue("");
-                                rootRef.child("Groups").child(groupName).child("Members").updateChildren(user);
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot2) {
+                                if (!dataSnapshot2./*hasChild(rootRef.child("Users").child(member).child(groupName).*/getKey().equals(groupName)) {
+                                    validated = true;
+                                    user = new HashMap<String, Object>();
+                                    user.put(member, 0);
+                                    rootRef.child("Users").child(member).child(groupName).setValue("");
+                                    rootRef.child("Groups").child(groupName).child("Members").updateChildren(user);
 
-                                Query queryRef = rootRef.child("Groups").child(groupName).child("Members");
-                                queryRef.addChildEventListener(new ChildEventListener() {
+                                    Query queryRef = rootRef.child("Groups").child(groupName).child("Members");
+                                    queryRef.addChildEventListener(new ChildEventListener() {
 
-                                    @Override
-                                    public void onChildAdded(DataSnapshot dataSnapshot3, String s) {
-                                        user = new HashMap<String, Object>();
-                                        user.put(dataSnapshot3.getKey().toString(), 0);
-                                        rootRef.child("Users").child(member).child(groupName).child("MemberRelations").updateChildren(user);
+                                        @Override
+                                        public void onChildAdded(DataSnapshot dataSnapshot3, String s) {
+                                            if (!dataSnapshot3.getKey().equals(member)) {
+                                                user = new HashMap<String, Object>();
+                                                user.put(dataSnapshot3.getKey().toString(), 0);
+                                                rootRef.child("Users").child(member).child(groupName).child("MemberRelations").updateChildren(user);
 
-                                        Map<String, Object> newUser = new HashMap<String, Object>();
-                                        newUser.put(member, 0);
-                                        rootRef.child("Users").child(dataSnapshot3.getKey()).child(groupName).child("MemberRelations").updateChildren(newUser);
-                                    }
+                                                Map<String, Object> newUser = new HashMap<String, Object>();
+                                                newUser.put(member, 0);
+                                                rootRef.child("Users").child(dataSnapshot3.getKey()).child(groupName).child("MemberRelations").updateChildren(newUser);
+                                                error = false;
+                                            }
 
-                                    @Override
-                                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                                        }
 
-                                    }
+                                        @Override
+                                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
 
-                                    @Override
-                                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                                        }
 
-                                    }
+                                        @Override
+                                        public void onChildRemoved(DataSnapshot dataSnapshot) {
 
-                                    @Override
-                                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                                        }
 
-                                    }
+                                        @Override
+                                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
 
-                                    @Override
-                                    public void onCancelled(FirebaseError firebaseError) {
+                                        }
 
-                                    }
-                                });
-                            } else {
-                                message = "The specified user is already part of the group.";
-                                error = true;
+                                        @Override
+                                        public void onCancelled(FirebaseError firebaseError) {
+
+                                        }
+                                    });
+                                } else {
+                                    //message = "The specified user is already part of the group.";
+                                    //error = true;
+                                    //Toast.makeText(context,"The specified user is already part of the group.",Toast.LENGTH_LONG).show();
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onCancelled(FirebaseError firebaseError) {
+                            @Override
+                            public void onCancelled(FirebaseError firebaseError) {
 
-                        }
-                    });
+                            }
+                        });
 
-                } else {
-                    message = "The specified user does not exist.";
-                    error = true;
-                }
+                    } else {
+                        //message = "The specified user does not exist.";
+                        //error = true;
+
+                        //Toast.makeText(context, "The specified user does not exist.", Toast.LENGTH_LONG).show();
+                    }
             }
 
             @Override
@@ -111,9 +122,12 @@ public class Utility {
 
             }
         });
-        if (!error)
-            message = "The specified user has been added to the group.";
-        return message;
+        if (!error) {
+            //message = "The specified user has been added to the group.";
+            //return message;
+
+            //Toast.makeText(context, "The specified user has been added to the group", Toast.LENGTH_LONG).show();
+        }
     }
 
     public static boolean addBill(Firebase rootRef, String groupName, String billName, int sum, String billCreator, Map<String, Object> members){
